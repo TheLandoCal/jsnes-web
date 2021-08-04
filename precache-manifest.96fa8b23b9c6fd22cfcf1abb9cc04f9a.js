a46a6e3cd7fbff6a074ea2607cdb5d36b3f14bbf
Lando Toothman <landortoothman@gmail.com>
2021-08-03 22:22:59 -0400

    Updates

---
+++ precache-manifest.96fa8b23b9c6fd22cfcf1abb9cc04f9a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05633951390f19e943d127f592928dfc",
+    "url": "/jsnes-web/index.html"
+  },
+  {
+    "revision": "399df86a29bd57211f37",
+    "url": "/jsnes-web/static/css/main.057be652.chunk.css"
+  },
+  {
+    "revision": "4eb5fa773aca8932958e",
+    "url": "/jsnes-web/static/js/2.6dcbd419.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/jsnes-web/static/js/2.6dcbd419.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "399df86a29bd57211f37",
+    "url": "/jsnes-web/static/js/main.1f0f60a0.chunk.js"
+  },
+  {
+    "revision": "fd848a91fab14049e0bc",
+    "url": "/jsnes-web/static/js/runtime-main.8273e804.js"
+  }
+]);

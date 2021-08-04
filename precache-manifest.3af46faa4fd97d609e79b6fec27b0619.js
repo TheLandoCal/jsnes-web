9125167b28748c8287b8836010dd7d7e99e4c965
Lando Toothman <landortoothman@gmail.com>
2021-08-03 22:11:52 -0400

    feat: Create GitHub Page branch

---
+++ precache-manifest.3af46faa4fd97d609e79b6fec27b0619.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b29aaa79ada5651a383dac147796e3b7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ab4a0432f30f3971d130",
+    "url": "/static/css/main.057be652.chunk.css"
+  },
+  {
+    "revision": "4eb5fa773aca8932958e",
+    "url": "/static/js/2.6dcbd419.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.6dcbd419.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab4a0432f30f3971d130",
+    "url": "/static/js/main.880fc4cc.chunk.js"
+  },
+  {
+    "revision": "dc6c9145439f3ba64f0c",
+    "url": "/static/js/runtime-main.be5a99a7.js"
+  }
+]);
